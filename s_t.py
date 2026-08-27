df5210bd39f114f8bc2eec2316a0ca3d8e715041
sebastianboteroz <sebastianboteroz2007@gmail.com>
2026-08-27 07:09:13 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -79,7 +79,7 @@ if result:
     text = str(result.get("GET_TEXT"))
     in_lang = st.selectbox(
         "Selecciona el lenguaje de Entrada",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Francés", "Alemán"),
     )
     if in_lang == "Inglés":
         input_language = "en"
@@ -93,10 +93,15 @@ if result:
         input_language = "zh-cn"
     elif in_lang == "Japonés":
         input_language = "ja"
+    elif in_lang == "Francés":
+        input_language = "fr"
+     elif in_lang == "Alemán":
+        input_language = "de"
+    
     
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés, "Francés", "Alemán""),
     )
     if out_lang == "Inglés":
         output_language = "en"
@@ -110,6 +115,10 @@ if result:
         output_language = "zh-cn"
     elif out_lang == "Japonés":
         output_language = "ja"
+     elif out_lang == "Francés":
+        output_language = "fr"
+     elif output_lang == "Alemán":
+        output_language = "de"
     
     english_accent = st.selectbox(
         "Selecciona el acento",
